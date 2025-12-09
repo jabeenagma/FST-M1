b0c24f09ffa325b1af700f7c4ee9169a91386cbb
nagma <36285704+jabeenagma@users.noreply.github.com>
2025-12-09 16:25:25 +0530

    activity21

---
+++ Python/Activities/activity21_test.py
@@ -0,0 +1,12 @@
+
+def test_sum():
+    assert 10 + 15 == 25
+
+def test_difference():
+    assert 30 - 15 == 10
+
+def test_product():
+    assert 4 * 4 == 16
+
+def test_quotient():
+    assert 8 / 8 == 1
